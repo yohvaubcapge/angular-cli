e92d0147a6a446607b368716c8953cc2f4a62529
Angular CLI <angular-cli@angular.io>
2017-06-29 09:33:38 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppAngularPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
